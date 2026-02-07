a7f5d7d229dcf34ea18792eacce7e816914d8e70
zejianzhuang-uv <zejian.zhuang@uv.es>
2026-02-07 11:30:55 +0100

    add Gaussian quadrature

--- src/Optimize.jl
+++ src/Optimize.jl
@@ -1,7 +1,8 @@
 module Optimize
 
 # Write your package code here.
-    export bisectv, brentqv
+    export bisectv, brentqv, gaussian_quad
     include("./bisect.jl")
     include("./brentq.jl")
+    include("./gaussian_quad.jl")
 end
