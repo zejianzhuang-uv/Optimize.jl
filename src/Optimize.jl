e58cc0e25d78044fa7280fa2f72fc8e70bf5f171
zejianzhuang-uv <zejian.zhuang@uv.es>
2026-05-19 15:09:05 +0200

    Update Project.toml and add STD.jl for statistical calculations

--- src/Optimize.jl
+++ src/Optimize.jl
@@ -1,9 +1,10 @@
 module Optimize
 
 # Write your package code here.
-    export bisectv, brentqv, gaussian_quad, brentqv_parallel, derivative
+    export bisectv, brentqv, gaussian_quad, brentqv_parallel, derivative, STD
     include("./bisect.jl")
     include("./brentq.jl")
     include("./gaussian_quad.jl")
     include("./derivative.jl")
+    include("STD.jl")
 end
---
+++ src/STD.jl
@@ -0,0 +1,27 @@
+
+
+
+import Statistics: std
+import Loess: loess, predict
+
+function loess_smooth(f::AbstractVector{Float64})
+    n = length(f)
+    predict(loess(Float64.(1:n), f), Float64.(1:n))
+end
+
+function loess_smooth(f::AbstractMatrix{Float64})
+    mapslices(loess_smooth, f, dims=1)
+end
+
+
+
+
+function STD(sample::AbstractArray; kwargs...)
+    err = std(sample, kwargs...)
+    if ndims(sample) == 3
+        err = err |> x -> dropdims(x, dims=3)
+    end
+    return loess_smooth(err)
+end
+
+
